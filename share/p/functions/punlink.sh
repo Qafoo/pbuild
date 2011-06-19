b7be369cdbedea398f4ec30d142b8c46915b9330
Jakob Westhoff <jakob@westhoffswelt.de>
2011-06-19 15:32:43 +0200

    Implemented punlink function

---
+++ share/p/functions/punlink.sh
@@ -0,0 +1,53 @@
+####
+# Copyright (c) 2011, Jakob Westhoff <jakob@westhoffswelt.de>
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+####
+
+##
+# Remove a linked file between the PHP_DIR and the INSTALL_DIR
+#
+# @param link
+##
+punlink() {
+    local link="$(makeRelativeTo "${D}" "$1")"
+
+    plog "Removing link: ${link}"
+
+    if [ -d "${D}/${link}" ]; then
+        perror "${D}/${link} is a directory but should be a file. Something odd is going on here."
+    fi
+
+    if [ -e "${D}/${link}" ] && [ ! -L "${D}/${link}" ]; then
+        if [ "$(pask "${D}/${link} is no symbolic link. Remove it anyways? [y/N]" "yn" "n")" != "y" ]; then
+            plog "Skipping removal of ${link} due to user request"
+            return
+        fi
+    fi
+
+
+
+    if [ -f "${D}/${link}" ]; then
+        rm "${D}/${link}"
+    fi
+}
