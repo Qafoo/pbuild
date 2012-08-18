c6093671e73ceaff44e5ec71281fc7c0499289da
Jakob Westhoff <jakob@westhoffswelt.de>
2012-08-18 17:57:31 +0200

    Added action to list available pbuilds

---
+++ share/pbuild/actions/list.sh
@@ -0,0 +1,45 @@
+####
+# Copyright (c) 2011, Jakob Westhoff <jakob@westhoffswelt.de>
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+####
+
+##
+# List all available pbuilds inside the PBUILD_DIR
+##
+action_list() {
+    if [ "$(ls -l "${PBUILD_DIR}/"*.pbuild 2>/dev/null| wc -l)" -le 1 ]; then
+        echo "There are currently no pbuild files available in the pbuild directory."
+        echo "Make sure '${PBUILD_DIR}' exists and is filled."
+    else
+        echo "The following pbuilds are ready to be used:"
+        local pbuild=""
+        for pbuild in "${PBUILD_DIR}/"*.pbuild; do
+            echo "* $(basename "${pbuild}" ".pbuild")"
+        done
+    fi
+
+    echo
+    echo "If you need help regarding the usage of this tool read the man page or"
+    echo "use the -h option"
+}
